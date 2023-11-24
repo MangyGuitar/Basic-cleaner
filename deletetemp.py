11a86c52cb0cfc1b7f837cbfa00a9d72607821b5
Mangy <114024328+MangyGuitar@users.noreply.github.com>
2023-11-24 18:23:03 +0100

    Add files via upload

---
+++ deletetemp.py
@@ -0,0 +1,16 @@
+import os
+from colorama import Fore, Back, just_fix_windows_console, Style
+
+
+os.system("title Delete Temp and Trash archives")
+
+print(Fore.LIGHTMAGENTA_EX + Style.BRIGHT + "I like Mexico XD")
+print(Fore.CYAN + Style.BRIGHT)
+os.system("rd %temp% /s /q")
+os.system("rd temp /s /q")
+os.system("ipconfig /flushdns")
+os.system("wuauclt /detectnow")
+os.system("wuauclt /updatenow")
+os.system("wuauclt /detectnow /updatenow")
+
+os.system("")
