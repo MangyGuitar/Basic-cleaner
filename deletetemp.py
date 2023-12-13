0aba3512100c32d8fddf1c69d0e45f497d287b78
Mangy <114024328+MangyGuitar@users.noreply.github.com>
2023-12-13 16:54:12 +0100

    Update deletetemp.py

--- deletetemp.py
+++ deletetemp.py
@@ -1,13 +1,44 @@
 import os
+import json
+import time
 
+nombre_archivo = os.path.join(os.path.dirname(__file__), 'config.json')
 
-os.system("title Delete Temp and Trash archives")
+with open(nombre_archivo, 'r') as archivo_json:
+    config = json.load(archivo_json)
 
-os.system("rd %temp% /s /q")
-os.system("rd temp /s /q")
-os.system("ipconfig /flushdns")
-os.system("wuauclt /detectnow")
-os.system("wuauclt /updatenow")
-os.system("wuauclt /detectnow /updatenow")
+# print(config)
 
-os.system("")
+class App:
+    def __init__(self):
+        self.printBanner()
+
+        os.system("title Delete Temp and Trash archives")
+        # time.sleep(1)
+        os.system("rd %temp% /s /q")
+        os.system("rd temp /s /q")
+        os.system("ipconfig /flushdns")
+        os.system("wuauclt /detectnow")
+        os.system("wuauclt /updatenow")
+        os.system("wuauclt /detectnow /updatenow")
+
+        os.system("")
+
+    def printBanner(self):
+        print(
+            """
+██████╗  █████╗  ██████╗██╗ █████╗       █████╗ ██╗      ███████╗ █████╗ ███╗   █╗███████╗██████╗ 
+██╔══██╗██╔══██╗██╔════╝██║██╔══██╗     ██╔══██╗██║      ██╔════╝██╔══██╗████╗ ██║██╔═══  ██╔══██╗
+██████╦╝███████║╚█████╗ ██║██║  ═╝█████╗██║  ╚═╝██║      █████╗  ███████║██╔██╗██║█████╗  ██████╔╝
+██╔══██╗██╔══██║ ╚═══██╗██║██║  █╗╚════╝██║  ██╗██║      ██╔══╝  ██╔══██║██║╚████║██╔══╝  ██╔══██╗
+██████╦╝██║  ██║██████╔╝██║╚█████╔╝      ╚█████╔╝███████╗███████╗██║  ██║██║ ╚███║███████╗██║  ██║
+╚═════╝ ╚═╝  ╚═╝╚═════╝ ╚═╝ ╚════╝        ╚════╝ ╚══════╝╚══════╝╚═╝  ╚═╝╚═╝  ╚══╝╚══════╝╚═╝  ╚═╝
+
+Github : https://github.com/MangyGuitar/Basic-cleaner
+
+------------------------------------------------------------------------
+"""
+        )
+
+if __name__ == "__main__":
+    app = App()
